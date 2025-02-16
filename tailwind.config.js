23c683b2bbc3807060eee37411ca3c261f803fa5
zehra <zehrakocairi@gmail.com>
2025-02-16 14:54:02 +0100

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/**/*.{html,ts}",
+    "./src/**/**/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      colors: {
+        primary: {
+          ...colors.indigo,
+        },
+        secondary: {
+          ...colors.rose,
+        },
+      },
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "32rem",
+        144: "36rem",
+      },
+    },
+  },
+  plugins: [],
+};
