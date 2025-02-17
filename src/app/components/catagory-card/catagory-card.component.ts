428d2a3825fb9fa0036ab1727bb1243a6231e79b
zehra <zehrakocairi@gmail.com>
2025-02-17 11:51:04 +0100

    complete category section

---
+++ src/app/components/catagory-card/catagory-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catagory-card',
+  imports: [],
+  templateUrl: './catagory-card.component.html',
+  styleUrl: './catagory-card.component.scss'
+})
+export class CatagoryCardComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,12 @@ import {
   Input,
 } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CatagoryCardComponent } from '../../components/catagory-card/catagory-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CatagoryCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
