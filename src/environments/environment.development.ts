61eaaaca0df04305e78446ecd50dc2b82917484b
zekeriya kocairi <zekeriyakocairi@gmail.com>
2025-12-06 23:38:46 +0100

    temp commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiUrl: 'BACKEND BASE URL COMES HERE',
+  serviceUrls: {
+    'catalog-api': 'http://localhost:5001',
+    'pricing-api': 'http://localhost:5002',
+    'basket-api': 'http://localhost:5003',
+    'ordering-api': 'http://localhost:5004',
+  },
+};
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'BACKEND BASE URL COMES HERE',
   serviceUrls: {
-    'catalog-api':'https://nest.lontray.shop/catalog-api',
+    'catalog-api': 'https://nest.lontray.shop/catalog-api',
     'basket-api': 'https://nest.lontray.shop/basket-api',
     'pricing-api': 'https://nest.lontray.shop/pricing-api',
     'ordering-api': 'https://nest.lontray.shop/ordering-api',
