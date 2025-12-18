1bf3c156c7ee251c2ca3c66d5756f3a06ae9010f
zekeriya kocairi <zekeriyakocairi@gmail.com>
2025-12-18 14:43:31 +0100

    Configures development environment and updates styles
    Configures the development environment with local service URLs for various APIs.
    This allows for easier local testing and development.
    
    Updates the frame options component's color to improve readability and
    visual consistency.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: false,
   apiUrl: 'BACKEND BASE URL COMES HERE',
-  // serviceUrls: {
-  //   'catalog-api': 'http://localhost:5001',
-  //   'pricing-api': 'http://localhost:5002',
-  //   'basket-api': 'http://localhost:5003',
-  //   'ordering-api': 'http://localhost:5004',
-  // },
   serviceUrls: {
-    'catalog-api': 'https://nest.lontray.shop/catalog-api',
-    'basket-api': 'https://nest.lontray.shop/basket-api',
-    'pricing-api': 'https://nest.lontray.shop/pricing-api',
-    'ordering-api': 'https://nest.lontray.shop/ordering-api',
+    'catalog-api': 'http://localhost:5001',
+    'pricing-api': 'http://localhost:5002',
+    'basket-api': 'http://localhost:5003',
+    'ordering-api': 'http://localhost:5004',
   },
+  // serviceUrls: {
+  //   'catalog-api': 'https://nest.lontray.shop/catalog-api',
+  //   'basket-api': 'https://nest.lontray.shop/basket-api',
+  //   'pricing-api': 'https://nest.lontray.shop/pricing-api',
+  //   'ordering-api': 'https://nest.lontray.shop/ordering-api',
+  // },
 };
