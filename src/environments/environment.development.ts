2448b2e74ec7f8aa90b514d2d669e47fdcfae5f6
zekeriya kocairi <zekeriyakocairi@gmail.com>
2026-01-01 14:16:58 +0100

    fix ci/cd

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'BACKEND BASE URL COMES HERE',
+  appInsightsConnectionString: '',
+  serviceUrls: {
+    'catalog-api': 'https://lontray.art/catalog-api',
+    'basket-api': 'https://lontray.art/basket-api',
+    'pricing-api': 'https://lontray.art/pricing-api',
+    'ordering-api': 'https://lontray.art/ordering-api',
+    bff: 'https://lontray.art/bff',
+  },
+};
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiUrl: 'BACKEND BASE URL COMES HERE',
+  appInsightsConnectionString:
+    'InstrumentationKey=1cd74c94-ea61-443c-9377-1bbcdb631b21;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=7ef6b9c5-c79e-4912-a29b-7ca3a3d7ddb6',
+  serviceUrls: {
+    'catalog-api': 'https://lontray.art/catalog-api',
+    'basket-api': 'https://lontray.art/basket-api',
+    'pricing-api': 'https://lontray.art/pricing-api',
+    'ordering-api': 'https://lontray.art/ordering-api',
+    bff: 'https://lontray.art/bff',
+  },
+};
